f9293ddd53290f5560a2bacc4f4e6b3ee9531a23
Powers <christopher.powers@accenture.com>
2017-12-06 15:46:25 -0700

    no arrays

--- src/List.ts
+++ src/List.ts
@@ -1,6 +1,10 @@
+interface Items {
+    [key: string]: any;
+}
+
 export default class List<T> {
     // private items: Array<T>;
-    private items: T[];
+    private items: Items = {};
 
     constructor(_items: T[] = []) {
         this.items = _items;
@@ -8,10 +12,52 @@ export default class List<T> {
 
     length(): number {
         let count: number = 0;
-        this.items.forEach((item: T) => {
-            count++;
-        });
+        for (let val in this.items) {
+            if (this.items.hasOwnProperty(val)) {
+                count++;
+            }
+        }
         return count;
+    }
+
+    forEach(callback: (x: T, y: number) => {}): void {
+        let count: number = 0;
+        for (let val in this.items) {
+            if (this.items.hasOwnProperty(val)) {
+                callback(this.items[val], count);
+                count++;
+            }
+        }
+    }
+
+    push(inp: T): number {
+        const currentLength: number = this.items.length();
+        this.items[currentLength + 1] = inp;
+        return this.length();
+    }
+
+    pop(): void {
+        if (this.items.length() === 0) {
+            return;
+        }
+        delete this.items[this.items.length()];
+    }
+
+    shift(): void {
+        if (this.items.length() === 0) {
+            return;
+        }
+        delete this.items[0];
+    }
+
+    indexOf(val: T): number {
+        let pos: number;
+        this.items.forEach((queryVal: T, index: number): void => {
+            if (val === queryVal) {
+                pos = index;
+            }
+        });
+        return pos;
     }
 
     splice(startIndex: number = undefined, take: number = 1): List<T> {
@@ -34,16 +80,5 @@ export default class List<T> {
         }
 
         return newArr;
-    }
-
-    forEach(callback: (x: T, y: number) => {}): void {
-        for (let i: number = 0; i < this.items.length; i++) {
-            callback(this.items[i], i);
-        }
-    }
-
-    push(inp: T): number {
-        this.items.push(inp);
-        return this.length();
     }
 }
