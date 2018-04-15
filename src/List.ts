9f516d2f35acd8f028daba19c96ca23f950825b0
Christopher Powers <christopher.powers@mortgagecadence.com>
2018-04-15 16:04:22 -0600

    Temp move to functional length

--- src/List.ts
+++ src/List.ts
@@ -1,84 +1,84 @@
 interface Items {
-    [key: string]: any;
+  [key: string]: any;
 }
 
 export default class List<T> {
-    // private items: Array<T>;
-    private items: Items = {};
+  private items: Array<T>;
+  // private items: Items = {};
 
-    constructor(_items: T[] = []) {
-        this.items = _items;
+  constructor(_items: T[] = []) {
+    this.items = _items;
+  }
+
+  Len(): number {
+    let count: number = 0;
+    for (let val in this.items) {
+      if (this.items.hasOwnProperty(val)) {
+        count++;
+      }
     }
+    return count;
+  }
 
-    length(): number {
-        let count: number = 0;
-        for (let val in this.items) {
-            if (this.items.hasOwnProperty(val)) {
-                count++;
-            }
+  forEach(callback: (x: T, y: number) => {}): void {
+    let count: number = 0;
+    for (let val in this.items) {
+      if (this.items.hasOwnProperty(val)) {
+        callback(this.items[val], count);
+        count++;
+      }
+    }
+  }
+
+  push(inp: T): number {
+    const currentLength: number = this.items.length;
+    this.items[currentLength + 1] = inp;
+    return this.Len();
+  }
+
+  pop(): void {
+    if (this.items.length === 0) {
+      return;
+    }
+    delete this.items[this.items.length];
+  }
+
+  shift(): void {
+    if (this.items.length === 0) {
+      return;
+    }
+    delete this.items[0];
+  }
+
+  indexOf(val: T): number {
+    let pos: number;
+    this.items.forEach((queryVal: T, index: number): void => {
+      if (val === queryVal) {
+        pos = index;
+      }
+    });
+    return pos;
+  }
+
+  splice(startIndex: number = undefined, take: number = 1): List<T> {
+    const newArr: List<T> = new List();
+    const endIndex: number = startIndex + take;
+
+    try {
+      let newItems: Array<T> = [];
+      this.items.forEach((item: T, index: number) => {
+        if (index >= startIndex && index < endIndex) {
+          newArr.push(item);
         }
-        return count;
-    }
-
-    forEach(callback: (x: T, y: number) => {}): void {
-        let count: number = 0;
-        for (let val in this.items) {
-            if (this.items.hasOwnProperty(val)) {
-                callback(this.items[val], count);
-                count++;
-            }
+        if (index < startIndex || index >= endIndex) {
+          newItems.push(item);
         }
+      });
+      this.items = newItems;
+    } catch (e) {
+      return e;
     }
 
-    push(inp: T): number {
-        const currentLength: number = this.items.length();
-        this.items[currentLength + 1] = inp;
-        return this.length();
-    }
-
-    pop(): void {
-        if (this.items.length() === 0) {
-            return;
-        }
-        delete this.items[this.items.length()];
-    }
-
-    shift(): void {
-        if (this.items.length() === 0) {
-            return;
-        }
-        delete this.items[0];
-    }
-
-    indexOf(val: T): number {
-        let pos: number;
-        this.items.forEach((queryVal: T, index: number): void => {
-            if (val === queryVal) {
-                pos = index;
-            }
-        });
-        return pos;
-    }
-
-    splice(startIndex: number = undefined, take: number = 1): List<T> {
-        const newArr: List<T> = new List();
-        const endIndex: number = startIndex + take;
-
-        try {
-            let newItems: Array<T> = [];
-            this.items.forEach((item: T, index: number) => {
-                if (index >= startIndex && index < endIndex) {
-                    newArr.push(item);
-                }
-                if (index < startIndex || index >= endIndex) {
-                    newItems.push(item);
-                }
-            });
-            this.items = newItems;
-        } catch (e) {
-            return e;
-        }
-
-        return newArr;
-    }
+    return newArr;
+  }
 }
--- test/List.test.ts
+++ test/List.test.ts
@@ -1,53 +1,53 @@
 import List from "../src/List";
 
 test("index", () => {
-    const list: List<number> = new List([1, 2, 3, 4]);
-    expect(list[0]).toBe(1);
+  const list: List<number> = new List([1, 2, 3, 4]);
+  expect(list[0]).toBe(1);
 });
 
 test("length", () => {
-    const list: List<any> = new List();
-    expect(list.length()).toBe(0);
+  const list: List<any> = new List();
+  expect(list.Len()).toBe(0);
 
-    const list2: List<any> = new List(["string"]);
-    expect(list2.length()).toBe(1);
+  const list2: List<any> = new List(["string"]);
+  expect(list2.Len()).toBe(1);
 });
 
 test("splice", () => {
-    const testList: Array<any> = [1, 2, "last"];
+  const testList: Array<any> = [1, 2, "last"];
 
-    const list1: List<any> = new List(testList);
-    expect(list1.splice(0).length()).toBe(1);
+  const list1: List<any> = new List(testList);
+  expect(list1.splice(0).Len()).toBe(1);
 
-    const list2: List<any> = new List(testList);
-    expect(list2.splice(0, 1).length()).toBe(1);
+  const list2: List<any> = new List(testList);
+  expect(list2.splice(0, 1).Len()).toBe(1);
 
-    const list3: List<any> = new List(testList);
-    expect(list3.splice(0, 2).length()).toBe(2);
+  const list3: List<any> = new List(testList);
+  expect(list3.splice(0, 2).Len()).toBe(2);
 });
 
 test("forEach", () => {
-    const list: List<number> = new List([1, 2, 3, 4]);
+  const list: List<number> = new List([1, 2, 3, 4]);
 
-    let test: number;
-    const testFunction: any = (a: any) => {
-        test = a;
-    };
+  let test: number;
+  const testFunction: any = (a: any) => {
+    test = a;
+  };
 
-    list.forEach(testFunction);
+  list.forEach(testFunction);
 
-    expect(test).toBe(4);
+  expect(test).toBe(4);
 });
 
 test("push", () => {
-    const list: List<any> = new List();
-    expect(list.push("string")).toBe(1);
+  const list: List<any> = new List();
+  expect(list.push("string")).toBe(1);
 });
 
 test("pop", () => {
-    const testList: Array<number> = [1, 2, 3];
-    const list: List<number> = new List(testList);
+  const testList: Array<number> = [1, 2, 3];
+  const list: List<number> = new List(testList);
 
-    expect(list.pop()).toBe(testList[testList.length - 1]);
-    expect(list.length()).toBe(testList.length - 1);
+  expect(list.pop()).toBe(testList[testList.length - 1]);
+  expect(list.Len()).toBe(testList.length - 1);
 });
